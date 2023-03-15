c9df26c7b1df53ca2b91e98d33dbbc03187cf827
krogo81611 <krogo81611@gmail.com>
2023-03-15 15:53:21 -0600

    Commit updates

--- FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
-import { Employee } from 'src/app/models/employee.model'
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
+
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.css']
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
 
-  employees: Employee[] = [
-        
-  ];
+  employees: Employee[] = [];
+  constructor(private employeesService: EmployeesService) {};
 
-  constructor() { }
+ 
 
   ngOnInit(): void {
-    
+    this.employeesService.getAllEmployees()
+    .subscribe({
+      next: (employees) => {
+        this.employees = employees;
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    })
   }
 }
--- FullStack.UI/src/environments/environment.ts
+++ FullStack.UI/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseApiUrl: 'https://localhost:7120/'
+    baseApiUrl: 'https://localhost:7120'
 }
