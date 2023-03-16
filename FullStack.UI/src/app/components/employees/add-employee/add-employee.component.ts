c0e7bf95cda14bceee00e6324965e7c6ebafd502
krogo81611 <krogo81611@gmail.com>
2023-03-16 16:03:02 -0600

    Commit Updates

--- FullStack.UI/src/app/components/employees/add-employee/add-employee.component.ts
+++ FullStack.UI/src/app/components/employees/add-employee/add-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 
 @Component({
@@ -19,14 +21,19 @@ export class AddEmployeeComponent implements OnInit{
     department: ''
   }
 
-  constructor() {}
+  constructor(private employeeService: EmployeesService, private router: Router) {}
   
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    
   }
 
   addEmployee() {
-    console.log(this.addEmployeeRequest);
+    this.employeeService.addEmployee(this.addEmployeeRequest)
+    .subscribe({
+      next: (employee) => {
+        this.router.navigate(['employees']);
+      }
+    });
   }
 
 }
--- FullStack.UI/src/app/services/employees.service.ts
+++ FullStack.UI/src/app/services/employees.service.ts
@@ -12,7 +12,17 @@ export class EmployeesService {
   baseApiUrl: string = environment.baseApiUrl;
 
   constructor(private http: HttpClient) { }
+
   getAllEmployees(): Observable<Employee[]> {
     return this.http.get<Employee[]>(this.baseApiUrl + '/api/employees');
   }
+
+  addEmployee(addEmployeeRequest: Employee): Observable<Employee> {
+     addEmployeeRequest.id = '00000000-0000-0000-0000-000000000000';
+     return this.http.post<Employee>(this.baseApiUrl + '/api/employees', addEmployeeRequest);
+  }
+  getEmployee(id: string): Observable<Employee> {
+    return this.http.get<Employee>(this.baseApiUrl + '/api/employees/' + id);
+  }
+  
 }
